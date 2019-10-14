a6efcb3d571402eb8f87b8ac480e22184d9d5e15
Harlexai <56288692+Harlexai@users.noreply.github.com>
2019-10-14 20:54:34 +0100

    Error Detections well commented for fix

---
+++ Server/SetUp.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Setting up database</title>
+  </head>
+  <body>
+
+    <h3>Setting up...</h3>
+<?php 
+  require_once 'Dependencies.php';
+ #How the Data would be stored in MySql database...
+  CreateTable('Comments',
+              ' Name VARCHAR(10),
+			    Phone INT,
+				Level INT(3),
+				LodgeName VARCHAR(255),
+				Location VARCHAR(255),
+				Description VARCHAR(1024),
+				Time TIMESTAMP DEFAULT CURRENT_TIMESTAMP');
+?>
+
+ <br>...done.
+  </body>
+</html>
